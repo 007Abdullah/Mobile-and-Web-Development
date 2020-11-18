2f70526a6cd353267525fd3c87dc87357b78634b
Muhammad Abdullah <kb337137@gmail.com>
2020-11-19 03:08:31 +0500

    Working

---
+++ JavaScript/My Practice/index.js
@@ -0,0 +1,23 @@
+alert("Hello World");
+
+window.alert( "Changes" );
+
+// variable and Strings
+
+var name = "Mark";
+alert(name);
+// rechaning name value
+name = "Ace";
+alert(name);
+
+var MyNactionality;
+// You can declare a variable in one statement, leaving it undefined. Then you can assign a
+// value to it in a later statement, without declaring it again
+MyNactionality = "U.S";
+
+alert(MyNactionality);
+
+
+var Thank = "Thanks for your inputs!";
+
+alert( Thank );
