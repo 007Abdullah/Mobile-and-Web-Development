0e8469dfee6b20044ce76a24016c382c2f766d27
Muhammad Abdullah <kb337137@gmail.com>
2020-11-20 01:27:58 +0500

    Addision-subract-etc-and-Operator-Precedence

---
+++ JavaScript/My Practice/Addision-subract-etc-and-Precedence .js
@@ -0,0 +1,26 @@
+    // let a = 20;  //21 /22 //23 //22
+
+    // let b = 4 + 5 - 10 + 5 - --a + a++ - 4 + ++a + a++;    
+    //  // let b = 42;
+   
+
+
+    // console.log("Result" , b , a);   
+
+    // multiplication
+   // let a = 10; //11 //10 //11 //10
+
+   // let b = 2 + 6 - 4 + 5 - --a + a++ - a-- + ++a + 10 * 20;
+    //  let b = 209;
+
+   // console.log("result" , b , a);
+
+    let a = 8; //9 //8 //9 //8
+    
+ //   let b = 10 + 20 - 10 + ++a - a-- + 20 + a++ * --a;
+    let b = 20 + 20 + 64;
+    
+// 104 // 8 Answer
+
+    console.log("Result :" , b , a);
+    
