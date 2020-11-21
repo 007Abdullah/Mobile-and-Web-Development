1971422f229f4cb02b7ba7a8751053416fc8bba0
Muhammad Abdullah <kb337137@gmail.com>
2020-11-21 06:26:54 +0500

    Calculation-Application-add-Etc

---
+++ JavaScript/My Practice/Calculation-Application.js
@@ -0,0 +1,10 @@
+function adds(){
+        var num1 = parseInt(document.getElementById("firstNum").value); 
+        var num2 = parseInt(document.getElementById("secondNum").value);
+        var sum = num1 + num2;
+        document.getElementById("result").innerHTML = sum;
+
+      
+}
+
+  
