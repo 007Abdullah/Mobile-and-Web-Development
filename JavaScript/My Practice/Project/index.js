a696d6f275557bfe4615a0833a1207ea4032f6dd
Muhammad Abdullah <kb337137@gmail.com>
2020-12-09 01:56:47 +0500

    sign up and login page with local storage

--- JavaScript/My Practice/Project/index.js
+++ JavaScript/My Practice/Project/index.js
@@ -1,24 +1,80 @@
 
-let arr = [];
-let email = "";
-let pass = "";
+// let arr = [];
+let email = [];
+let pass = [];
 function DataStore() {
     let name = document.getElementById("name").value;
-    localStorage.setItem("name", name);
-    email = document.getElementById("email").value;
-    localStorage.setItem("email", email);
-    pass = document.getElementById("password").value;
-    localStorage.setItem("Passoerd", pass);
-    let male = document.getElementById("male").checked;
-    let female = document.getElementById("female").checked;
-    if (female) {
-        arr.push("Name :" + name, "Email :" + email, "Password :" + pass, "female :" + female);
-    }
-    else if (male) {
-        arr.push("Name :" + name, "Email :" + email, "Password :" + pass, "male :" + male);
-    }
+    // localStorage.setItem("name", name);
+    email.push(document.getElementById("email").value);
+    pass.push(document.getElementById("password").value);
+    // let male = document.getElementById("male").checked;
 
-    console.log(arr);
+    localStorage.setItem("email", JSON.stringify(email));
+    localStorage.setItem("Passoerd", JSON.stringify(pass));
+
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+
+    // let female = document.getElementById("female").checked;
+    // if (female) {
+    //     arr.push("Name :" + name, "Email :" + email, "Password :" + pass, "female :" + female);
+    // }
+    // else if (male) {
+    //     arr.push("Name :" + name, "Email :" + email, "Password :" + pass, "male :" + male);
+    // }
+
+    //window.location.href = "login.html";
+    console.log(email);
+    console.log(pass)
+    return false;
 }
 
 
+// login 
+
+// document.getElementById("result").innerHTML = localStorage.getItem("name");
+
+
+
+function login() {
+    const Username = document.getElementById("emails").value;
+    const Password = document.getElementById("passwords").value;
+
+    let emails = JSON.parse(localStorage.getItem("email"));
+    let passwords = JSON.parse(localStorage.getItem("Passoerd"));
+    let isFound = false;
+
+    console.log(emails);
+    console.log(passwords);
+
+    
+    for (let i = 0; i < emails.length; i++) {
+        if (Username === emails[i]) {
+            isFound = i;
+            break;
+        }
+    }
+    if (isFound === false) {
+        alert("user not found")
+    } else if (passwords[isFound] === Password) {
+
+        alert("login success");
+        window.location.href = "./check.html"
+       
+    } 
+    else {
+        alert("Error Password or Username")
+    }
+
+
+    // if (emails === Username && passwords === Password) {
+    //     console.log("WelCome");
+    // }
+    // else {
+    //     console.log("Invalid");
+    // }
+    
+    return false;
+    
+}
